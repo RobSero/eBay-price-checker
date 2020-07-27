b7b7f94901bc1067bd06c739a6fd142638c1f7a9
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-07-27 10:55:13 +0100

    Backend - Used regex to remove prices within a range, returns average price

--- static/server/scraper.py
+++ static/server/scraper.py
@@ -1,10 +1,11 @@
 import webbrowser
 import bs4
+import re
 import requests
 import pprint #for testing purposes only, cleaner list printing
 
 #  get string from searchbar
-user_input = 'dynasty warrior 5 empires'
+user_input = 'face mask'
 # if spaces, replace with '+'
 search_item = user_input.replace(' ', '+')
 # listing type: Buy it now, auction only etc.
@@ -19,15 +20,35 @@ print(res.status_code)
 
 # if len > 0, scape data
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
-# get item titles, quality and price
+# get item titles, quality
 element_titles = soup.select('.s-item__title')
-element_prices = soup.select('.s-item__price')
 element_quality = soup.select('.SECONDARY_INFO')
-# test print data
 
-# build data tuples
-results = []
-for i in range(len(element_titles)):
-  results.append((element_titles[i].text, element_prices[i].text, element_quality[i].text))
+# Get item prices & format
+prices_list_strings = soup.select('.s-item__price')
+subRegex = re.compile(r'£')
+element_prices = []
+for price in prices_list_strings:
+  if 'to' in price.text: # ex. £3.99 to £ 4.99
+    # split string into list with two values, remove '£', convert to floats and find average
+    split_prices = price.text.split(' to ')
+    average_price = sum([float(subRegex.sub('', split_prices[0])), float(subRegex.sub('', split_prices[1]))]) / 2
+    element_prices.append(average_price)
+  else:
+    element_prices.append(float(subRegex.sub('', price.text)))
+
+
+
+# element_prices =  [float(subRegex.sub('', price.text)) for price in prices_list_strings]
+
+
+# # build data tuples
+# results = []
+# for i in range(len(element_titles)):
+#   results.append((element_titles[i].text, element_prices[i], element_quality[i].text))
   
-pprint.pprint(results)
+  
+  
+# average_price = round(sum(element_prices) / len(element_prices), 2)
+# print(average_price)
+# # pprint.pprint(results)
