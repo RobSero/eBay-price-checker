0114b8627d3167903a5f8914f3fd74baa97f4cf7
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-08-04 10:26:05 +0100

    deployment edits

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, send_from_directory, send_file
-from flask_restful import Resource, Api
+# from flask_restful import Resource, Api
 from flask_cors import CORS
 from static.server.scraper import result_generator
 from static.server.excelwriter import createSpreadsheet
@@ -16,83 +16,33 @@ def homeView():
     return '<h1>Hello Chapss</h1>'
 
 
-
-# #  search result api
-# @app.route('/api/search/<input>')
-# def scrape_results(input):
-#   return result_generator(input=input)
-
+# class scrape_results(Resource):
+#     def get(self, userinput):
+#         results = result_generator(input=userinput)  
+#         return results
 
 
 
-# #  excel spreadsheet api
-# @app.route('/api/send/')
-# def send_spreadsheet():
-#   return createSpreadsheet(ebayData)
-
-#  search result api
-# @app.route('/api/search/<input>')
-# def scrape_results(input):
-#   return result_generator(input=input)
-
-
-class scrape_results(Resource):
-    def get(self, userinput):
-        results = result_generator(input=userinput)  
-        return results
-
-
-
-class create_results(Resource):
-  # createspreadsheet
-    def post(self):
-        print('recieved')
-        user_email = request.json['name']
-        print(user_email)
-        createSpreadsheet(request.json)
-        print(f'{request.json["name"]}_eBayData.xlsx')
-        return {'message' : f'{request.json["name"]}'}
+# class create_results(Resource):
+#   # createspreadsheet
+#     def post(self):
+#         print('recieved')
+#         user_email = request.json['name']
+#         print(user_email)
+#         createSpreadsheet(request.json)
+#         print(f'{request.json["name"]}_eBayData.xlsx')
+#         return {'message' : f'{request.json["name"]}'}
      
-        #  send spreadsheet
+#         #  send spreadsheet
     
         
-class send_results(Resource):
-  def get(self, filename):
-        print('recieved')
-        return send_file(f'./sheets/{filename}_eBayData.xlsx', as_attachment=True)
+# class send_results(Resource):
+#   def get(self, filename):
+#         print('recieved')
+#         return send_file(f'./sheets/{filename}_eBayData.xlsx', as_attachment=True)
 
 
 
-# api.add_resource(scrape_results, '/')
-api.add_resource(scrape_results, '/api/search/<string:userinput>')
-api.add_resource(create_results, '/api/send')
-api.add_resource(send_results, '/api/send/<string:filename>')
-
-jsonData = {
-  "average_price": 109.68,
-  "user_email" : "john@email.com",
-  "image": "https://i.ebayimg.com/thumbs/images/g/VG0AAOSwqwRdxs7E/s-l225.jpg",
-  "name": "tombi 2",
-  "results": [
-    [
-      "Name 1",
-      74.99,
-      "Pre-owned",
-      "https://www.ebay.co.uk/itm/PlayStation-1-Tombi-2-Rental-Copy-Rare-Collectors-Item/353109158363?hash=item5236f229db%3Ag%3AY3MAAOSwUQBe6PzJ&amp;LH_BIN=1"
-    ],
-    
-    [
-      "Name 2",
-      74.99,
-      "Pre-owned",
-      "https://www.ebay.co.uk/itm/PlayStation-1-Tombi-2-Rental-Copy-Rare-Collectors-Item/353109158363?hash=item5236f229db%3Ag%3AY3MAAOSwUQBe6PzJ&amp;LH_BIN=1"
-    ],
-    
-    [
-      "Name 3",
-      74.99,
-      "Pre-owned",
-      "https://www.ebay.co.uk/itm/PlayStation-1-Tombi-2-Rental-Copy-Rare-Collectors-Item/353109158363?hash=item5236f229db%3Ag%3AY3MAAOSwUQBe6PzJ&amp;LH_BIN=1"
-    ]
-  ]
-}
+# api.add_resource(scrape_results, '/api/search/<string:userinput>')
+# api.add_resource(create_results, '/api/send')
+# api.add_resource(send_results, '/api/send/<string:filename>')
