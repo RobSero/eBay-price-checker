a524252e110d3c6a017981a333c35f52164c656e
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-08-04 10:38:12 +0100

    c

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, send_from_directory, send_file
-# from flask_restful import Resource, Api
+from flask_restful import Resource, Api
 # from flask_cors import CORS
 from static.server.scraper import result_generator
 from static.server.excelwriter import createSpreadsheet
