9ac5929915ab6f06649b2f2d8986b56b415fbf2b
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-08-05 11:09:13 +0100

    cors

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, send_from_directory, send_file, render_template
 from flask_restful import Resource, Api
-# from flask_cors import CORS
+from flask_cors import CORS
 from static.server.scraper import result_generator
 from static.server.excelwriter import createSpreadsheet
 
 app = Flask(__name__)
 # app.run(debug=True)
 api = Api(app)
-# CORS(app)
-# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route('/')
